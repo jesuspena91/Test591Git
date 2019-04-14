8d9ff69467f59da5424ab14584401f57c40a5f45
jesusp <jesus@pseas.upenn.edu>
2019-04-14 16:30:47 -0500

    Use lines 7 + 8 for conflict resolution and send screenshot (jesusp@seas.upenn.edu)

--- Test591Git/src/TestClass.java
+++ Test591Git/src/TestClass.java
@@ -4,5 +4,7 @@ public static void main(String[] args) {
 	System.out.println();
 	System.out.println("adding a test output here");
 	// Adding a comment for an additional test
+	System.out.println("Line of code for conflict "
+			+ "(please send screenshot)!");
 }
 }
